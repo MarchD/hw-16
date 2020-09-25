151b39bf886ba33561425d25af1519c5c07fa386
mdubchak <dubchmar@gmail.com>
2020-09-25 11:53:44 +0300

    Updates

---
+++ precache-manifest.c5f8ceb6effb31840442923ec308d351.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9807b1465583ecf4c3f0556edff09347",
+    "url": "/hw-16/index.html"
+  },
+  {
+    "revision": "52fa790fac7e378dedb4",
+    "url": "/hw-16/static/css/main.09020f64.chunk.css"
+  },
+  {
+    "revision": "96e86e6dc7d06734964f",
+    "url": "/hw-16/static/js/2.0febfca1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw-16/static/js/2.0febfca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52fa790fac7e378dedb4",
+    "url": "/hw-16/static/js/main.e5323ddf.chunk.js"
+  },
+  {
+    "revision": "c15aa6a83206202bbb4d",
+    "url": "/hw-16/static/js/runtime-main.5f3faf15.js"
+  },
+  {
+    "revision": "e5ca8c0ac474df46fe45840707a0c483",
+    "url": "/hw-16/static/media/RobotoMono-Regular.e5ca8c0a.ttf"
+  },
+  {
+    "revision": "616584840ae17a262b42a419b33f5e6a",
+    "url": "/hw-16/static/media/phone.61658484.svg"
+  }
+]);
